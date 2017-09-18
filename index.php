cf639fea8c9304a280b4e6a10393d0e1d2dfac30
Evan Pratten <ewpratten@gmail.com>
2017-09-18 17:03:12 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<head>
+
+</head>
+<body>
+
+
+
+<script src="https://www.gstatic.com/firebasejs/4.3.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCtD5cyvWijUyDm4xRbvazVm5k_5k2DYkE",
+    authDomain: "clicc-bd5d6.firebaseapp.com",
+    databaseURL: "https://clicc-bd5d6.firebaseio.com",
+    projectId: "clicc-bd5d6",
+    storageBucket: "clicc-bd5d6.appspot.com",
+    messagingSenderId: "448687478158"
+  };
+  firebase.initializeApp(config);
+</script>
+</body>
